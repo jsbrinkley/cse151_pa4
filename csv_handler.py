790e505295965734d1cdf0fb30970c37289313d2
James Brinkley <jsbtons7@gmail.com>
2016-05-20 20:50:21 -0700

    Current Progress after Friday Night

--- csv_handler.py
+++ csv_handler.py
@@ -2,34 +2,18 @@ import math
 import random
 import numpy as np
 
-def getTrainingSet(indexArray, rng):
-    obsvLeft = float(418)
-    dataLeft = float(4177)
 
-    lineCount = 0
-
-    with open("abalone.data", "r+") as file:
-        for line in file:
-            randThresh = rng.random()
-            if randThresh < obsvLeft/dataLeft:
-                indexArray[lineCount] += 1
-                obsvLeft -= 1
-
-            if obsvLeft == 0:
-                break
-
-            dataLeft -= 1
-            lineCount += 1
-
-def getMeanStd(indexArray):
-    mean = np.mean(indexArray)
-    std = np.std(indexArray, dtype=np.float64)
+def get_mean_std(index_array):
+    mean = np.mean(index_array)
+    std = np.std(index_array, dtype=np.float64)
     return mean, std
+
 
 def apply_z_scale(training_set, mean_array, std_array):
     for element in training_set:
         for i in range(0, len(mean_array) - 1):
             element[i] = (element[i] - mean_array[i]) / std_array[i]
+
 
 def get_k_neighbors(training_set, test_data, k):
     k_nearest_neighbors = [(0, 0)] * k
@@ -52,3 +36,53 @@ def get_k_neighbors(training_set, test_data, k):
         for element in k_nearest_neighbors:
             nearest_neighbors_data.append(training_set[element[0]])
         return nearest_neighbors_data
+
+
+def get_average(data_set):
+    mean_array = []
+
+    for i in range(0, len(data_set[0]) - 1):
+        attribute_array = []
+        for j in range(0, len(data_set)):
+            observation = data_set[j]
+            attribute_array.append(observation[i])
+        mean = np.mean(attribute_array)
+        mean_array.append(mean)
+
+    return mean_array
+
+
+def get_index_of_new_centroid(cluster):
+    mean_array = get_average(cluster)
+    centroid_index = get_closest(cluster, mean_array)
+    return centroid_index
+
+
+def get_closest(cluster, mean_array):
+    # return index of closest element in data_set to the average
+    closest_index = 0
+
+    # return index of set that the data_entry will be switched too
+    for i in range(0, len(cluster)):
+        if get_euclidean_distance(cluster[i], mean_array) < \
+                get_euclidean_distance(cluster[closest_index], mean_array):
+            closest_index = i
+    return closest_index
+
+
+def get_nearest_centroid_index(cluster_list, data_entry):
+    closest_centroid_index = 0
+
+    # return index of set that the data_entry will be switched too
+    for i in range(0, len(cluster_list)):
+        if get_euclidean_distance(cluster_list[i][0], data_entry) < \
+                get_euclidean_distance(data_entry, cluster_list[closest_centroid_index][0]):
+            closest_centroid_index = i
+    return closest_centroid_index
+
+
+def get_euclidean_distance(point_a, point_b):
+    distance = 0
+    for j in range(0, len(point_a) - 1):
+        distance += math.pow((point_a[j] - point_b[j]), 2)
+    return math.sqrt(distance)
--- pa4.py
+++ pa4.py
@@ -1,7 +1,8 @@
-
 import csv
 import numpy as np
 import random as rng
+import csv_handler
+
 '''
 RMSEs
 regression-0.05.csv:
@@ -27,7 +28,6 @@ with open("abalone.data", "r+") as file:
         data = []
         sex_index = True
         for element in line.split(','):
-            # SEX INDEX ONLY FOR ABALONE, must comment out if doing miscategorization
             if sex_index:
                 sex_index = False
                 if element == "M" or element == "m":
@@ -47,15 +47,76 @@ with open("abalone.data", "r+") as file:
         whole_data.append(data)
 
 
-# whole_data[i] = np.asarray(whole_data[i])
-
 np.random.seed(99)
 np.random.shuffle(whole_data)
-train_num = len(whole_data)
+train_num = int(len(whole_data) * 0.9)
 
-print train_num
+training_set = whole_data[:train_num]
+test_set = whole_data[train_num:]
+
+mean_array = []
+std_array = []
+
+# TODO EXCLUDING LAST COLUMN -- -UNSURE
+for i in range(0, len(training_set[0]) - 1):
+    attribute_array = []
+    for j in range(0, len(training_set)):
+        observation = training_set[j]
+        attribute_array.append(observation[i])
+    mean = np.mean(attribute_array)
+    std = np.std(attribute_array)
+    mean_array.append(mean)
+    std_array.append(std)
+
+csv_handler.apply_z_scale(training_set, mean_array, std_array)
+csv_handler.apply_z_scale(test_set, mean_array, std_array)
+
+k = 4
+# get k centroids method
+#      - shuffle the set
+#      - pick top K entries...these are our first centroids
+np.random.shuffle(training_set)
+
+# make K number of lists to hold clusters
+#   - centroids always at front of set list
+clusters_list = [[] for x in range(0, k)]
+
+for i in range(0, len(clusters_list)):
+    clusters_list[i].append(training_set.pop(0))
+
+# Step through entire training dataset
+#   - assign each entry to the cluster who's centroid it is closest too
+for i in range(0, len(training_set)):
+    data_point = training_set.pop(0)
+    nearest_centroid_index = csv_handler.get_nearest_centroid_index(clusters_list, data_point)
+    clusters_list[nearest_centroid_index].append(data_point)
+
+# Recalculate the centroid for each cluster by taking the average of the cluster
+#   - Step through each cluster and reassign each entry to the the cluster who's new centroid it is closest too
+element_switched = True
+while element_switched:
+    element_switched = False
+    for i in range(0, len(clusters_list)):
+        index_of_new_centroid = csv_handler.get_index_of_new_centroid(clusters_list[i])
+        clusters_list[i].insert(0, clusters_list.pop(index_of_new_centroid))
+
+    for i in range(0, len(clusters_list)):
+        original_size = len(clusters_list[i])
+
+        for j in range(1, original_size):
+            data_point = clusters_list[i].pop(1)
+            nearest_centroid_index = csv_handler.get_nearest_centroid_index(clusters_list, data_point)
+
+            if nearest_centroid_index != i:
+                element_switched = True
+            clusters_list[nearest_centroid_index].append(data_point)
+
+# Repeat the step above until no entry changes cluster
 
 
+# Calculate and print WCSS and Centroid
+
+#
 '''
 data = np.array([[float(r) for r in row] for row in whole_data])
 
